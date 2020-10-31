d684f2db570624fe1c203fb3d65e4c6aadb440ad
taniczhang <zxkr777@163.com>
2020-10-31 13:26:19 +0800

    Update redistools.go

--- redistools.go
+++ redistools.go
@@ -1,4 +1,4 @@
-package redis
+package redistools
 
 import (
 	"fmt"
@@ -19,8 +19,8 @@ type Cache struct {
 // 返回cache 对象, 在多个工具之间建立一个 中间初始化的时候使用
 func NewRedisCache(db int, host string, defaultExpiration time.Duration) Cache {
 	pool := &redis.Pool{
-		MaxActive:   100,                              //  最大连接数，即最多的tcp连接数，一般建议往大的配置，但不要超过操作系统文件句柄个数（centos下可以ulimit -n查看）
-		MaxIdle:     100,                              // 最大空闲连接数，即会有这么多个连接提前等待着，但过了超时时间也会关闭。
+		MaxActive:   10,                              //  最大连接数，即最多的tcp连接数，一般建议往大的配置，但不要超过操作系统文件句柄个数（centos下可以ulimit -n查看）
+		MaxIdle:     10,                              // 最大空闲连接数，即会有这么多个连接提前等待着，但过了超时时间也会关闭。
 		IdleTimeout: time.Duration(100) * time.Second, // 空闲连接超时时间，但应该设置比redis服务器超时时间短。否则服务端超时了，客户端保持着连接也没用
 		Wait:        true,                             // 当超过最大连接数 是报错还是等待， true 等待 false 报错
 		Dial: func() (redis.Conn, error) {
